b0199710e2a44805a6a85a921134bca70fd9a1fd
Andres Lopez <andres.lopez@unmsm.edu.pe>
2019-02-25 07:14:57 -0500

    first commit

---
+++ server/src/index.ts
@@ -0,0 +1,39 @@
+import express, { Application } from "express";
+import morgan from 'morgan';
+import cors from 'cors';
+
+import indexRoutes from './routes/indexRoutes';
+
+class Server {
+    
+    public app:Application;
+
+    constructor(){
+        this.app = express();
+        this.config();
+        this.routes();
+        this.start();
+    }
+
+    config(): void{
+        this.app.set('port', process.env.PORT || 3000);
+        //MORGAN PERMITE VER LAS PETICIONES QUE SE REALIZAN EN LA CONSOLA DESPUES QUE SE REALIZAN
+        this.app.use(morgan('dev'));
+        this.app.use(cors());
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: false }));
+    }
+
+    routes():void {
+        //LLAMA A LAS RUTAS DEL INDEXROUTES QUE SUS DIRECC CORREN A PARTIR DE ESTA DIRECCION
+        this.app.use('/api/platillos',indexRoutes);
+    }
+
+    start():void {
+        this.app.listen(this.app.get('port'), ()=>{
+            console.log(`Server on port`, this.app.get('port'));
+        });
+    }
+
+}
+const server = new Server();
