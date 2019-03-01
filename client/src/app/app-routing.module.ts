02c665b8e9162160c56ab8a45bd9d6209a91cc93
Anthony Diaz <anthony.diaz2@unmsm.edu.pe>
2019-02-28 23:12:48 -0500

    Commit arreglado los tslint y con los componentes children

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,39 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { Navigation1Component } from './components/navigation1/navigation1.component';
+import { Main1Component } from './components/main1/main1.component';
+
+import { Navigation2Component } from './components/navigation2/navigation2.component';
+import { Main2Component } from './components/main2/main2.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/index',
+    pathMatch: 'full'
+  },
+  {
+    path: 'index',
+    component: Navigation1Component,
+    children: [
+      {
+        path: '',
+        component: Main1Component
+      }
+    ]
+  },
+  {
+    path: 'armaTuPedido',
+    component: Navigation2Component,
+    children: [
+      {
+        path: '',
+        component: Main2Component
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/components/main1/main1.component.ts
+++ client/src/app/components/main1/main1.component.ts
@@ -1,63 +1,64 @@
 import { Component, OnInit } from '@angular/core';
-declare var jQuery:any;
-declare var $:any;
+declare var jQuery: any;
+declare var $: any;
 
 @Component({
   selector: 'app-main1',
   templateUrl: './main1.component.html',
-  styleUrls: ['./main1.component.css']
+  styleUrls: ['./main1.component.css',
+              '../../../book2.css']
 })
+
+/* tslint:disable */
 export class Main1Component implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
-    
-  $(document).ready(function(){
+  $(document).ready( () => {
 
-    $(window).scroll(function(){
-      var windowWidth = $(window).width();
-  
-      if(windowWidth > 800){
-        var scroll = $(window).scrollTop();
-  
+    $(window).scroll( () => {
+      const windowWidth = $(window).width();
+      if( windowWidth > 800 ) {
+        const scroll = $(window).scrollTop();
+
         $('header .textos').css({
-          'transform': 'translate(0px,' + scroll / 2 + '%)'
+          transform: 'translate(0px,' + scroll / 2 + '%)'
         });
-  
+
         $('.acerca-de article').css({
-          'transform': 'translate(0px, -' + scroll / 4 + '%)'
+          transform: 'translate(0px, -' + scroll / 4 + '%)'
         });
       }
     });
-  
-    $(window).resize(function(){
-      var windowWidth = $(window).width();
+
+    $(window).resize( () => {
+      const windowWidth = $(window).width();
       if(windowWidth < 800) {
         $('.acerca-de article').css({
-          'transform': 'translate(0px, 0px)'
+          transform: 'translate(0px, 0px)'
         });
       }
     });
-    
-// Efecto Menu
-	$('.menu a').each(function(index, elemento){
-		$(this).css({
-			'top': '-200px'
+
+    // Efecto Menu
+    $('.menu a').each(function(index: any , elemento: any){
+      $(this).css({
+        top: '-200px'
+    });
+
+      $(this).animate({
+        top: '0'
+      }, 2000 + (index * 500));
+    });
+
+    // Efecto Header
+    if ( $(window).width() > 800 ) {
+      $('header .textos').css({
+        opacity: 0,
+        marginTop: 0
 		});
-
-		$(this).animate({
-			top: '0'
-		}, 2000 + (index * 500));
-	});
-
-// Efecto Header
-	if( $(window).width() > 800 ){
-		$('header .textos').css({
-			opacity: 0,
-			marginTop: 0
-		});
-
+		
 		$('header .textos').animate({
 			opacity: 1,
 			marginTop: '-52px'
@@ -65,88 +66,88 @@ export class Main1Component implements OnInit {
 	}
 
 // Scroll Elementos Menu
-	var acercaDe = $('#acerca-de').offset().top,
+	   var acercaDe = $('#acerca-de').offset().top,
 		menu = $('#platillos').offset().top,
 		galeria = $('#galeria').offset().top,
 		ubicacion = $('#ubicacion').offset().top;
 
-	$('#btn-acerca-de').on('click', function(e){
+	   $('#btn-acerca-de').on('click', function(e: any) {
 		e.preventDefault();
 		$('html, body').animate({
 			scrollTop: acercaDe - 100
 		}, 500);
 	});
 
-	$('#btn-menu').on('click', function(e){
+	   $('#btn-menu').on('click', function(e) {
 		e.preventDefault();
 		$('html, body').animate({
 			scrollTop: menu
 		}, 500);
 	});
 
-	$('#btn-galeria').on('click', function(e){
+	   $('#btn-galeria').on('click', function(e) {
 		e.preventDefault();
 		$('html, body').animate({
 			scrollTop: galeria
 		}, 500);
 	});
 
-	$('#btn-ubicacion').on('click', function(e){
+	   $('#btn-ubicacion').on('click', function(e) {
 		e.preventDefault();
 		$('html, body').animate({
 			scrollTop: ubicacion
 		}, 500);
 	});
 
-	//Efecto about
-	document.getElementById("about").addEventListener('click', function(e){
-		//alert("Platillo 1");
+	// Efecto about
+	   document.getElementById('about').addEventListener('click', function(e) {
+		// alert("Platillo 1");
 		e.preventDefault();
-		
+
 		$('html, body').animate({
 			scrollTop: 0
 		}, 1000);
-		
-		//FUNCION PARA EFECTO BLUR
+
+		// FUNCION PARA EFECTO BLUR
 		$({blurRadius: 0}).animate({blurRadius: 10}, {
 			easing: 'linear',
-			step: function() {
+			step() {
 				$('body').css({
-					"-webkit-filter": "blur("+this.blurRadius+"px)",
-					"filter": "blur("+this.blurRadius+"px)"
+					'-webkit-filter': 'blur(' + this.blurRadius + 'px)',
+					filter: 'blur(' + this.blurRadius + 'px)'
 				});
 			}
 		}, 1000);
 
 		// CAMBIO DE PAGINA SPA
-		setTimeout(function(){ 
-						
+		setTimeout(function() {
+
 			$({blurRadius: 10}).animate({blurRadius: 0}, {
 				easing: 'linear',
-				step: function() {
+				step() {
 					$('body').css({
-						"-webkit-filter": "blur("+this.blurRadius+"px)",
-						"filter": "blur("+this.blurRadius+"px)"
+						'-webkit-filter': 'blur(' + this.blurRadius + 'px)',
+						filter: 'blur(' + this.blurRadius + 'px)'
 					});
 				}
 			}, 2000);
 
-			
-		location.href = "./index2.html"; 
+
+		 location.href = './index2.html';
 		}, 1500);
 	});
 
-	//Efectos platillos
-	document.getElementById("platillo-1").addEventListener('click', function(e){
-		//alert("Platillo 1");
+	// Efectos platillos
+	   document.getElementById('platillo-1').addEventListener('click', function(e) {
+		// alert("Platillo 1");
 		window.open('http://www.google.com', '_blank');
-		//QUE ABRA LA PAGINA AUXILIAR PERO DE ESE PLATO Y QUE SE PUEDA REGRESAR A LA PAGINA PRINCIPAL O 
-		//ARMA TU PLATO
+		// QUE ABRA LA PAGINA AUXILIAR PERO DE ESE PLATO Y QUE SE PUEDA REGRESAR A LA PAGINA PRINCIPAL O
+		// ARMA TU PLATO
 	});
 
-	//Efecto galeria
-	document.getElementById("foto-1").addEventListener('click', function(e){
-		//alert("Platillo 1");
+	// Efecto galeria
+	   document.getElementById('foto-1').addEventListener('click', function(e) {
+		// alert("Platillo 1");
 		window.open('http://www.google.com', '_blank');
 	});
 
