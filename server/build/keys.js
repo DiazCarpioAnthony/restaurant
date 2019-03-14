0cf010b21b1dfe23ca1c7cf9aa98d1e64eb5ee6b
Anthony Diaz <anthony.diaz2@unmsm.edu.pe>
2019-03-14 00:00:59 -0500

    Con BD remota y pluggin ClearBD

--- server/build/keys.js
+++ server/build/keys.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'restaurant_bd'
+        host: 'us-cdbr-iron-east-03.cleardb.net',
+        user: 'be2914381dcbde',
+        password: '49266f47',
+        database: 'heroku_7a9bbc6433f4d8c'
     }
 };
